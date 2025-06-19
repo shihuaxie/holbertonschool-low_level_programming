489b806d346f0ebb1866125db50b0f6ece9cf27f
shihuaxie <sylvia501628@gmail.com>
2025-06-19 02:07:17 +0000

    Add task 3 to evaluate if it's a lowercase or uppercase char

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _islower - checks if a character is lowercase
+ *
+ * @c: the character to check
+ *
+ *  Return: 1 if lowercase; 0 if uppercase.
+ */
+
+int _islower(int c)
+{
+
+	if (c >= 97 &&  c <= 122)
+
+		return (1);
+
+	else
+
+		return (0);
+}
