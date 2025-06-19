0ac2904343732d660c71d220ce2d07cd0c766220
shihuaxie <sylvia501628@gmail.com>
2025-06-19 00:33:41 +0000

    Add task 2 to print out lowercase letters for 10 times

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - function to print out lowercase alphabet for 10 times.
+ *
+ * Return: void.
+ */
+
+void print_alphabet_x10(void)
+{
+	int n;
+
+	char c;
+
+	for (n = 0; n >= 9; n++)
+	{
+		for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
+
+		_putchar('\n');
+
+	}
+}
