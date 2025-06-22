537ec0c21a12dbb80928e402e74a6dcb1ca1ce82
shihuaxie <sylvia501628@gmail.com>
2025-06-22 05:25:13 +0000

    Add task 7 files

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,15 @@
+#include "main.c"
+
+/**
+ * _print_last_digit - Function that returns the last digit value of an integer.
+ *
+ * @n: - the number value to evaluate.
+ *
+ * Return: returns the last digit of the input nubmer.
+ *
+ */
+
+int print_last_digit(int n)
+{
+
+}
