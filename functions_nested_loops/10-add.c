f849e2d2b633d6cce6c484d34cf5504dd40083f0
shihuaxie <sylvia501628@gmail.com>
2025-06-22 14:23:42 +0000

    Add task 10 to prints the sum of two integers

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * add - Prints the sum of two ints.
+ *
+ * @x: The first param
+ * @y: The second param
+ *
+ * Return: The sum of the two integers.
+ */
+
+int add(int x, int y)
+{
+	int sum;
+
+	sum = (x + y);
+
+	return (sum);
+}
