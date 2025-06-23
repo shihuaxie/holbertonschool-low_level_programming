acb08631e8b05c94ace04cef779620bb8e8d56ab
shihuaxie <sylvia501628@gmail.com>
2025-06-23 11:21:51 +0000

    Add task 7 to print out diagonal line

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * print_diagonal - Function to print out diagonal line
+ *
+ * @n: the number of times the line to be printed
+ *
+ * Return: void
+ */
+
+void print_diagonal(int n)
+{
+	int i, j;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (i = 0; i < n; i++)
+		{
+			for (j = 0; j < i; j++)
+			{
+				_putchar(' ');
+			}
+			_putchar('\\');
+			_putchar('\n');
+		}
+	}
+}
