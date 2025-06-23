a30f5be7b75ffc44b27d3c080d201a4fb7eb52ae
shihuaxie <sylvia501628@gmail.com>
2025-06-23 11:38:42 +0000

    Update if else loop to while loop

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -10,7 +10,7 @@
 
 void print_diagonal(int n)
 {
-	int i, j;
+	int i = 0, j;
 
 	if (n <= 0)
 	{
@@ -18,14 +18,17 @@ void print_diagonal(int n)
 	}
 	else
 	{
-		for (i = 0; i < n; i++)
+		while (i < n)
 		{
-			for (j = 0; j < i; j++)
+			j = 0;
+			while (j < i)
 			{
 				_putchar(' ');
+				j++;
 			}
 			_putchar('\\');
 			_putchar('\n');
+			i++;
 		}
 	}
 }
