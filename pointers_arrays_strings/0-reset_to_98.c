1c7b5cac8de58f1178c7cec442ea807401b17782
shihuaxie <sylvia501628@gmail.com>
2025-06-25 02:28:11 +0000

    Add task 0 to update the pointer value to 98

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - Function to update the point value to 98.
+ *
+ * @n: the pointer to be passed in.
+ *
+ * Return: void
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
