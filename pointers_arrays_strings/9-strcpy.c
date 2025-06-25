5c956af75c3f668f88e8d35cf4ae04d29f96fc14
shihuaxie <sylvia501628@gmail.com>
2025-06-25 11:30:40 +0000

    Add task 9 to copy string src to string dest

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _strcpy - function copies the string to dest, including \0
+ * @dest: first param
+ * @src: second param
+ *
+ * Return: always 0
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+/** assign the individual elements from src to dest one by one */
+	while (src[i] != 0)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+/** copy the \0 as well */
+	dest[i] = '\0';
+
+	return dest;
+}
