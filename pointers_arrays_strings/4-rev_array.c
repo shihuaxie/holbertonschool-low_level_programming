0ebabf8afdf1b0bd0899779baa6127ec52577ca9
shihuaxie <sylvia501628@gmail.com>
2025-06-29 05:16:43 +0000

    Add task 4 files

---
+++ pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * reverse_array - Function reverses the content of an array of integers.
+ * @a: The array needs to be reversed
+ * @n: The amount of the array
+ *
+ * Return: A reversed array
+ *
+ */
+
+void reverse_array(int *a, int n)
+{
+	
+}
