04411130f992458e37ed964ec299c569fd7dff6f
shihuaxie <sylvia501628@gmail.com>
2025-06-29 09:58:24 +0000

    Add task 4 to reverse an array

--- pointers_arrays_strings/4-rev_array.c
+++ pointers_arrays_strings/4-rev_array.c
@@ -11,5 +11,18 @@
 
 void reverse_array(int *a, int n)
 {
-	
+	int start = 0; /** it points to the first ele in a */
+	int end = n - 1; /** it points to the last ele in a */
+	int tmp;
+
+	while (start < end)
+	{
+		tmp = a[start];
+		a[start] = a[end];
+		a[end] = tmp;
+
+/** start and end will compare with each other from 2 sides to middle */
+		start++;
+		end--;
+	}
 }
