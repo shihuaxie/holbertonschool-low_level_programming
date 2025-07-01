e0598999b6a8b9bd9e623b70054f8eaf0bd5d387
shihuaxie <sylvia501628@gmail.com>
2025-07-01 02:49:07 +0000

    Add task 0 to print out a string

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -1,14 +1,23 @@
 #include "main.h"
 
 /**
+ * _puts_recursion - Function to print out a string.
+ * @s: pointer to the first string element
  *
- *
- *
- *
+ * Return: void
  *
  */
 
 void _puts_recursion(char *s)
 {
-
+	if (*s == '\0') /** base case: end of string */
+	{
+		_putchar('\n');
+		return;
+	}
+	else
+	{
+		_putchar(*s); /** print current character */
+		_puts_recursion(s + 1); /** recuse to the next character */
+	}
 }
