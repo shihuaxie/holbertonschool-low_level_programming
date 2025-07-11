59c8292a6229331c6579bff105d977c2316cd31d
shihuaxie <sylvia501628@gmail.com>
2025-07-11 14:16:17 +0000

    Fix: replace strlen function

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -16,16 +16,19 @@
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *new_string;
-	unsigned int len_s2;
-	unsigned int len_s1;
+	unsigned int len_s2 = 0;
+	unsigned int len_s1 = 0;
 	unsigned int i, j;
 
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
-	len_s1 = strlen(s1);
-	len_s2 = strlen(s2);
+
+	while (s1[len_s1]!= '\0')
+		len_s1++;
+	while (s2[len_s2] != '\0')
+		len_s2++;
 
 	if (n >= len_s2)
 	{
