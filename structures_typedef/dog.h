9cb56decf1ecad3d7d4d07df122d6d4dde8f62e2
shihuaxie <sylvia501628@gmail.com>
2025-07-13 07:53:13 +0000

    Add task 0 to define a struct dog type

---
+++ structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - defined new type
+ * @name: pointer to string
+ * @age: a float
+ * @owner: pointer to string
+ *
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
