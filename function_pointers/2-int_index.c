637c027210293da0c19ac641a9d85887a44e6818
shihuaxie <sylvia501628@gmail.com>
2025-07-14 14:53:07 +0000

    Optimize the code to make it clean

--- function_pointers/2-int_index.c
+++ function_pointers/2-int_index.c
@@ -15,22 +15,17 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i = 0;
 
-	if (array != NULL && cmp != NULL)
+	if (array == NULL && cmp == NULL && size <= 0)
+		return (-1);
+
+	for (i = 0; i < size; i++)
 	{
-		if (size <= 0)
+		if (cmp(array[i]) != 0)
 		{
-			return (-1);
-		}
-		else
-		{
-			for (i = 0; i < size; i++)
-			{
-				if (cmp(array[i]) != 0)
-				{
-					return (i);
-				}
-			}
+			/* if find an integer, return it */
+			return (i);
 		}
 	}
+	/* if no integer, returns-1 */
 	return (-1);
 }
