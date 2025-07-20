1f54dfa621aa45774c5a3ff35b3b351a4ebfa597
shihuaxie <sylvia501628@gmail.com>
2025-07-20 13:18:49 +0000

    Fix: add space to meet betty style

--- variadic_functions/2-print_strings.c
+++ variadic_functions/2-print_strings.c
@@ -27,7 +27,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 		else
 			printf("%s", str);
 
-		if (separator != NULL && i < n -1)
+		if (separator != NULL && i < n - 1)
 			printf("%s", separator);
 	}
 	va_end(args);
