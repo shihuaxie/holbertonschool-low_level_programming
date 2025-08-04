3b2cbb94a2f32c1fe25a397b731f0df15969b241
shihuaxie <sylvia501628@gmail.com>
2025-08-04 11:25:15 +0000

    Add task 3 file

---
+++ hash_tables/3-hash_table_set.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <string.h>
+#include "hash_tables.h"
+
+/**
+ * hash_table_set - Adds or updates a key/value pair to the hash table
+ * @ht: The hash table
+ * @key: The key (cannot be an empty string)
+ * @value: The value associated with the key (must be duplicated)
+ *
+ * Return: 1 if succeeded, 0 otherwise
+ */
+
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+
+}
