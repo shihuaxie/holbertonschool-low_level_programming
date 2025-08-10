0cd22ebc3841e5fefb89bd0ede9c11e12c68fe38
shihuaxie <sylvia501628@gmail.com>
2025-08-10 04:22:01 +0000

    Add task 3 that adds an element to the hash table

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -1,17 +1,57 @@
-#include <stdlib.h>
-#include <string.h>
 #include "hash_tables.h"
+#include <string.h>
+#include <stdlib.h>
 
 /**
- * hash_table_set - Adds or updates a key/value pair to the hash table
+ * hash_table_set - Adds or updates an element in the hash table
  * @ht: The hash table
- * @key: The key (cannot be an empty string)
- * @value: The value associated with the key (must be duplicated)
+ * @key: The key (non-empty string)
+ * @value: The value (can be empty string, will be duplicated)
  *
- * Return: 1 if succeeded, 0 otherwise
+ * Return: 1 if it succeeded, 0 otherwise
  */
-
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
+	unsigned long int index;
+	hash_node_t *new_node, *current;
+	char *value_copy;
 
+	if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
+		return (0);
+
+	value_copy = strdup(value);
+	if (value_copy == NULL)
+		return (0);
+
+	index = key_index((const unsigned char *)key, ht->size);
+	current = ht->array[index];
+	/* Check if key already exists, -> update value*/
+	while (current != NULL)
+	{
+		if (strcmp(current->key, key) == 0)
+		{
+			free(current->value);
+			current->value = value_copy;
+			return (1);
+		}
+		current = current->next;
+	}
+	/* Key not found → insert new node at head */
+	new_node = malloc(sizeof(hash_node_t));
+	if (new_node == NULL)
+	{
+		free(value_copy);
+		return (0);
+	}
+	new_node->key = strdup(key);
+	if (new_node->key == NULL)
+	{
+		free(value_copy);
+		free(new_node);
+		return (0);
+	}
+	new_node->value = value_copy;
+	new_node->next = ht->array[index];
+	ht->array[index] = new_node;
+	return (1);
 }
